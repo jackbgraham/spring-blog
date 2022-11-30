20aff2cae88f8c1336b2002689f0665d50175dc6
Jack Graham <jack.blair.graham@gmail.com>
2022-11-30 09:37:21 -0600

    Post.java updated

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,12 +1,74 @@
 package com.codeup.springblog.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="posts")
 public class Post {
 
-    private String title(){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, length = 50)
+    private String title;
 
+    @Column(nullable = false, length = 500)
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    public String getTitle() {
+        return title;
     }
 
-    public String body(){
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post() {
+    }
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post(String title, String body, User user) {
+        this.title = title;
+        this.body = body;
+        this.user = user;
     }
 }
